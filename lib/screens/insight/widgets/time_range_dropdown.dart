f1a20821890a4267f87e086444db6b41192226c3
tranght0o <minnari2004@gmail.com>
2025-11-14 16:00:38 +0700

    Refactor TimeRangeDropdown by removing options
    Removed unused time range options and formatted the constructor.

--- lib/screens/insight/widgets/time_range_dropdown.dart
+++ lib/screens/insight/widgets/time_range_dropdown.dart
@@ -4,10 +4,7 @@ enum TimeRange {
   today,
   thisWeek,
   thisMonth,
-  lastMonth,
-  last6Months,
   thisYear,
-  lastYear,
   allTime,
   custom,
 }
@@ -16,7 +13,11 @@ class TimeRangeDropdown extends StatelessWidget {
   final TimeRange selected;
   final Function(TimeRange) onChanged;
 
-  const TimeRangeDropdown({super.key, required this.selected, required this.onChanged});
+  const TimeRangeDropdown({
+    super.key,
+    required this.selected,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +35,18 @@ class TimeRangeDropdown extends StatelessWidget {
 
   String _label(TimeRange r) {
     switch (r) {
-      case TimeRange.today: return "Today";
-      case TimeRange.thisWeek: return "This Week";
-      case TimeRange.thisMonth: return "This Month";
-      case TimeRange.lastMonth: return "Last Month";
-      case TimeRange.last6Months: return "Last 6 Months";
-      case TimeRange.thisYear: return "This Year";
-      case TimeRange.lastYear: return "Last Year";
-      case TimeRange.allTime: return "All Time";
-      case TimeRange.custom: return "Custom Range";
+      case TimeRange.today:
+        return "Today";
+      case TimeRange.thisWeek:
+        return "This Week";
+      case TimeRange.thisMonth:
+        return "This Month";
+      case TimeRange.thisYear:
+        return "This Year";
+      case TimeRange.allTime:
+        return "All Time";
+      case TimeRange.custom:
+        return "Custom Range";
     }
   }
 }
