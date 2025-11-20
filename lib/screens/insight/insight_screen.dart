face2c6d4a74ef5a11e7c9ae05ab901f4b8418a0
Mint0306 <135294022+Mint0306@users.noreply.github.com>
2025-11-20 22:08:33 +0700

    Improved Insight Screen Layout

--- lib/screens/insight/insight_screen.dart
+++ lib/screens/insight/insight_screen.dart
@@ -4,21 +4,29 @@ import 'widgets/habit_chart.dart';
 import 'widgets/mood_chart.dart';
 import 'widgets/insight_summary.dart';
 
-
 class InsightScreen extends StatelessWidget {
   const InsightScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Insights")),
+      appBar: AppBar(
+        title: const Text("Insights"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      backgroundColor: Colors.grey.shade50,
       body: SingleChildScrollView(
         child: Column(
           children: const [
+            SizedBox(height: 8),
             InsightSummary(),
+            SizedBox(height: 16),
             TodoChart(),
+            SizedBox(height: 8),
             HabitChart(),
+            SizedBox(height: 8),
             MoodChart(),
+            SizedBox(height: 16),
           ],
         ),
       ),
